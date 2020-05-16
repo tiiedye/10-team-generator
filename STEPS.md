6e6237400de1fe52c05108d412f39eefe36fe273
Julia Clements <tiiedye@gmail.com>
2020-05-16 12:31:57 -0600

    wrote create manager function

---
+++ STEPS.md
@@ -0,0 +1,4 @@
+1. moved necessary files from the UCLA repo
+2. installed npm files
+3. began work on the lib folder, starting with employee.
+    NOTE: Ask instructor if the code given to us in the Master folder is meant to be copied or used as an example????
--- app.js
+++ app.js
@@ -14,6 +14,72 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const teamMembers = [];
+const idArray = [];
+
+function appMenu() {
+
+    function createManager() {
+        console.log("Please build your team");
+        inquirer
+            .prompt ([
+                {
+                    type: "input",
+                    name: "managerName",
+                    message: "What is your Manager's name?",
+                    validate: answer => {
+                        if (answer !== "") {
+                            return true;
+                        } else {
+                            return "Please enter at least one character.";
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "managerId",
+                    message: "What is your Manager's id?",
+                    validate: answer => {
+                        if (answer > 0) {
+                            return true;
+                        } else {
+                            return "Please enter a number greater than zero.";
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "managerEmail",
+                    message: "What is your Manager's email address?",
+                    validate: answer => {
+                        if (answer !== "") {
+                            return true;
+                        } else {
+                            return "Please enter a valid email address.";
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "managerOfficeNumber",
+                    message: "What is your Manager's office number?",
+                    validate: answer => {
+                        if (answer > 0) {
+                            return true;
+                        } else {
+                            return "Please enter a number greater than zero.";
+                        }
+                    }
+                }
+            ]).then(answers => {
+                const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+                teamMembers.push(manager);
+                idArray.push(answers.managerId);
+                createTeam();
+            })
+    }
+}
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
